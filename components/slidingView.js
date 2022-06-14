2cb04c4684cbbf012bae9233dce8ee66fbecd6d8
Nuhel <nuheljft@gmail.com>
2022-06-14 18:08:36 +0600

    Sliding View Issue Fixed. Module Complited

--- components/slidingView.js
+++ components/slidingView.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SlidingView = () => {
     let boundLogger;
     const test = (e) => {
+        return;
         let self = e.currentTarget;
         let parent = self.closest('div.sliding-parent')
         boundLogger = handelMouseMove.bind(null,self);
@@ -15,12 +16,21 @@ const SlidingView = () => {
         return false; 
     }
     
-    const handelMouseMove = (cursor,e) => {
-        let slidingBar = cursor.closest('div.slider-bar')
+    const handelMouseMove = (e) => {
+        let parent =  e.currentTarget;
+        
+        let slidingBar = parent.querySelector('div.slider-bar')
+        
+        let view1 = parent.querySelector('.view-1')
+        let view2 = parent.querySelector('.view-2')
+
         slidingBar.style.left = e.pageX+"px";
+        view1.style.clip = "rect(auto, auto, auto, "+e.pageX+"px)" 
+        view2.style.clip = "rect(auto, "+e.pageX+"px, auto, auto)" 
     }
     
     const test2 = (e) => {
+        return;
         let self = e.currentTarget;
         let parent = self.closest('div.sliding-parent')
         parent.removeEventListener('mousemove',boundLogger,true)
@@ -29,9 +39,9 @@ const SlidingView = () => {
     return (
         <div>
             <h1>Hello</h1>
-            <div className='parent relative sliding-parent'>
+            <div className='parent relative sliding-parent overflow-hidden' onMouseMove={handelMouseMove}>
                 <img src='image 2.png ' className='view-1 w-full '></img>
-                <img src='image 3.png' className='view-2  w-full absolute h-full top-0'></img>
+                <img src='image 3.png' className='view-2  w-full absolute h-full top-0 left-0'></img>
                 <div  className='slider-bar bg-red-900  absolute h-full top-0' style={{ width: 5+"px"}}>
                     <div onMouseDown={test} onMouseUp={test2} className='slider rounded-full bg-black absolute' style={{ width: 15+"px", height: 15+"px" , top: "calc(50% - 7px)",  left: "calc(50% - 7px)"}}>
 
