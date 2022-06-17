501b8e2d57ac9f2a7f103fe7a75d5f3de1a608ab
jubairZamna01 <jubair.zaman01@gmail.com>
2022-06-17 12:58:25 +0600

    homepageUpdate

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import BannerPart from '../components/HomePage/BannerPart'
+import Itssoeasy from '../components/HomePage/Itssoeasy'
 import Whytakethelong from '../components/HomePage/Whytakethelong'
 import styles from '../styles/Home.module.css'
 
@@ -8,7 +9,9 @@ export default function Home() {
   return (
     <div>
       <BannerPart></BannerPart>
+      <Itssoeasy></Itssoeasy>
       <Whytakethelong></Whytakethelong>
+
 
 
 
