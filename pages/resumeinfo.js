f2edd84e24d4aa68d5c4ddf2dc0d25ed246ec473
Nuhel <nuheljft@gmail.com>
2022-06-19 14:55:18 +0600

    Cv template issue fixed.

--- pages/resumeinfo.js
+++ pages/resumeinfo.js
@@ -1,66 +1,29 @@
 import React, { useState } from 'react';
+import Cv1 from '../components/cvTemplets/Cv1';
 import Resumeform from '../components/ResumeBulder/Resumeform';
 
 const resumeinfo = () => {
-    const [firstname, setfirstname] = useState();
-    const [lastName, setlastName] = useState();
-    const [profession, setProfession] = useState();
+    const [cv, setCvData] = useState({
+        firstName: "",
+        lastName: "",
+    });
 
 
-    const handlefirstname = (value) => {
-        const inputvalue = value;
-        setfirstname(inputvalue);
-
-    }
-    const handlelastName = (value) => {
-        const inputvalue = value;
-        setlastName(inputvalue);
-
-    }
-    const handleProfession = (value) => {
-        const inputvalue = value;
-        setProfession(inputvalue);
-
+    const handelCvData = (value, name) => {
+        setCvData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
     }
 
 
     return (
         <div>
-            <Resumeform></Resumeform>
-
-
-
-
-
-            <div class="grid grid-cols-2">
-                <div className='bg-white'>
-                    <label>
-                        First Name :
-                    </label>
-                    <input className='border-solid border-2 border-indigo-600 ' type='text' onChange={(e) => handlefirstname(e.target['value'])} ></input>
-                    <br></br>
-                    <label>
-                        Last Name :
-                    </label>
-
-                    <input className='border-solid border-2 border-indigo-600 ' type='text' onChange={(e) => handlelastName(e.target['value'])} ></input>
-                    <br></br>
-                    <label>
-                        profession :
-                    </label>
-
-                    <input className='border-solid border-2 border-indigo-600 ' type='text' onChange={(e) => handleProfession(e.target['value'])} ></input>
-                </div>
-
-                <div className='bg-slate-200'>
-                    <h1> Name :{firstname}{" "}{lastName}</h1>
-                    <br></br>
-                    <h1>Profession : {profession}</h1>
-                </div>
+            <pre>{JSON.stringify(cv, null, 2)}</pre>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4  '>
+            <Resumeform handelCvData={handelCvData}></Resumeform>
+            <Cv1 cv={cv}/>
             </div>
-
-
-
         </div>
     );
 };
