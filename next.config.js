eb87237d2278f618b82ee978665a4af7cf680f46
jubairZamna01 <jubair.zaman01@gmail.com>
2022-06-23 18:39:31 +0600

    configjs

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
+
+
