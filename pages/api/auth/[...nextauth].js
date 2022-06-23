4f5ce7be9d82454adbb1e39160dc4657abd4be3c
jubairZamna01 <jubair.zaman01@gmail.com>
2022-06-23 21:12:42 +0600

    nextauthupdate

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+// import { PrismaAdapter } from "@next-auth/prisma-adapter"
+// import { PrismaClient } from "@prisma/client"
 const bcrypt = require('bcrypt');
 
 const prisma = new PrismaClient()
