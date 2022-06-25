fd8b12b301ff44641c9bd00a1ad31dd6d623c473
jubairZamna01 <jubair.zaman01@gmail.com>
2022-06-26 00:06:41 +0600

    CV2

--- components/cvTemplets/Cv2.js
+++ components/cvTemplets/Cv2.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Cv2 = ({cv}) => {
+const Cv2 = ({ cv }) => {
     return (
         <div>
 
             <div className='w-10/12 mx-auto bg-slate-50 rounded-lg my-10'>
                 <span>T 2</span>
-                <h1 className='text-md text-blue text-center font-semibold'>{cv.firstName??""}{" "}{cv.lastName??""}</h1>
-                <p className='text-center text-sm'>{cv.profession??""}</p>
+                <h1 className='text-md text-blue text-center font-semibold'>{cv.firstName ?? ""}{" "}{cv.lastName ?? ""}</h1>
+                <p className='text-center text-sm'>{cv.profession ?? ""}</p>
 
             </div>
 
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import DashmainNav from '../components/Deshboard/DashmainNav';
 import DashNav from '../components/Deshboard/DashNav';
 import Innerpart1 from '../components/Deshboard/innerpart1';
+import Navbar from '../components/navbar';
 
 const Dashboard = () => {
     return (
@@ -12,5 +14,12 @@ const Dashboard = () => {
         </div>
     );
 };
-
+// Dashboard.getLayout = function getLayout(page) {
+//     return (
+//         <>
+//             <div className='fixed z-50 w-full top-0'> <DashmainNav></DashmainNav> </div>
+//             {page}
+//         </>
+//     )
+// }
 export default Dashboard;
