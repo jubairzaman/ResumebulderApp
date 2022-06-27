67dace0a7db97d9edab28aa238b980a50445b798
jubairZamna01 <jubair.zaman01@gmail.com>
2022-06-27 13:29:21 +0600

    skills updated

--- components/ResumeBulder/skils.js
+++ components/ResumeBulder/skils.js
@@ -14,38 +14,7 @@ const Skils = () => {
                         <input onChange={(e) => handelCvData(e.target['value'], 'skill-1')} type="text" name="website" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="" />
                     </label>
                 </div>
-                <div>
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            skill-2
-                        </span>
-                        <input onChange={(e) => handelCvData(e.target['value'], 'skill-2')} type="text" name="website" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="" />
-                    </label>
-                </div>
-                <div>
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            skill-3
-                        </span>
-                        <input onChange={(e) => handelCvData(e.target['value'], 'skill-3')} type="text" name="website" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="" />
-                    </label>
-                </div>
-                <div>
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            skill-4
-                        </span>
-                        <input onChange={(e) => handelCvData(e.target['value'], 'skill-4')} type="text" name="website" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="" />
-                    </label>
-                </div>
-                <div>
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            skill-5
-                        </span>
-                        <input onChange={(e) => handelCvData(e.target['value'], 'skill-5')} type="text" name="website" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="" />
-                    </label>
-                </div>
+
 
             </div>
 
