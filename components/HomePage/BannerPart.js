f6ffb40b4ccbf0ebef37f33a78bcb7da2b11ede6
Nuhel <nuheljft@gmail.com>
2022-08-10 01:33:42 +0600

    sec try

--- components/HomePage/BannerPart.js
+++ components/HomePage/BannerPart.js
@@ -3,7 +3,7 @@ import Modal from '../modal'
 import Link from "next/link";
 
 import React, { useState } from "react";
-
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 const BannerPart = () => {
 
     const [showModal, setShowModal] = useState(false);
@@ -15,6 +15,8 @@ const BannerPart = () => {
     const show = () => {
         setShowModal(true);
     };
+
+    const hash1 = '1';
     return (
         <div className='bg-slate-50'>
 
@@ -33,7 +35,83 @@ const BannerPart = () => {
                 </section>
                 <div className='flex justify-center py-10 '>
                     <span className='rounded-lg bg-indigo-700 text-white px-10 py-5 '>
-                        <Modal></Modal>
+                    <>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <h1 onClick={show}>Create Resume</h1>
+
+      </ClickAwayListener>
+
+      {showModal ? (
+        <>
+
+
+          <>
+
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none" >
+              <div className="relative w-auto my-6 mx-auto  overflow-y-auto h-3/4 w-3/4  ">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                    <h3 className="text-xl text-black font-semibold">
+                      Pick your template to start . . .
+                    </h3>
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      onClick={() => setShowModal(false)}
+                    >
+                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        ×
+                      </span>
+                    </button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative p-6 flex-auto ">
+                    <div className="grid grid-cols-3 gap-4">
+                      <div className="drop-shadow-2xl ">
+                        <Link href={"cv-builder/" + hash1}>
+                          <img className='' src='cvimages/cv1.jpg'></img>
+                        </Link>
+                      </div>
+                      <div className="hover:transition-all drop-shadow-2xl">
+                        <Link href={"cv-builder/2"}>
+                          <img className='' src='cvimages/cv6.jpg'></img>
+                        </Link>
+                      </div>
+                      <div className="drop-shadow-2xl">
+                        <Link href={"cv-builder/3"}>
+                          <img className='' src='cvimages/cv3.jpg'></img>
+                        </Link>
+                      </div>
+                      <div className="drop-shadow-2xl">
+                        <Link href={"cv-builder/4"}>
+                          <img className='' src='cvimages/cv2.jpg'></img>
+                        </Link>
+                      </div>
+                      <div className="drop-shadow-2xl"><Link href={"cv-builder/1"}>
+                        <img className='' src='cvimages/cv4.jpg'></img>
+                      </Link></div>
+                      <div className="drop-shadow-2xl"><Link href={"cv-builder/1"}>
+                        <img className='' src='cvimages/cv5.jpg'></img>
+                      </Link></div>
+                      <div className="drop-shadow-2xl"><Link href={"cv-builder/1"}>
+                        <img className='' src='cvimages/cv7.jpg'></img>
+                      </Link></div>
+                      <div className="drop-shadow-2xl"><Link href={"cv-builder/1"}>
+                        <img className='' src='cvimages/cv8.jpg'></img>
+                      </Link></div>
+                    </div>
+                  </div>
+                  {/*footer*/}
+                </div>
+              </div>
+            </div>
+
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        </>
+      ) : null}
+    </>
                     </span>
                 </div>
                 <div className='flex justify-center  '> <div className="w-3 h-3 mr-2 bg-green-500 rounded-full flex my-auto  "></div>
